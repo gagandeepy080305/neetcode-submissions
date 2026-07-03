29bd1cd54a1edd06fa06b37a29353cc0fba81729
Gagandeep Y <gagan080305@gmail.com>
2026-07-03 20:31:39 +0530

    Add: daily-temperatures - submission-1

---
+++ Data Structures & Algorithms/daily-temperatures/submission-1.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    vector<int> dailyTemperatures(vector<int>& temperatures) {
+        // brute force
+        int n = temperatures.size();
+        vector<int>res(n,0);
+        for(int i=0;i<n;i++){
+            for(int j=i+1;j<n;j++){
+                if(temperatures[i]<temperatures[j]){
+                    res[i] = j-i;
+                    break;
+                }
+            }
+        }
+        return res;
+    }
+};
