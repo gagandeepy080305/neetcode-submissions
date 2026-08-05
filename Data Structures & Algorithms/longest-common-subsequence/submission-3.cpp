96947b5153c41e27a033ccac97cf4f269c77e968
Gagandeep Y <gagan080305@gmail.com>
2026-08-05 13:31:05 +0530

    Add: longest-common-subsequence - submission-3

---
+++ Data Structures & Algorithms/longest-common-subsequence/submission-3.cpp
@@ -0,0 +1,28 @@
+class Solution {
+public:
+    // Space Optimized Solution
+    // T.C. - O(n * m)
+    // S.C. - O(m)
+
+    int longestCommonSubsequence(string text1, string text2) {
+        int n = text1.size();
+        int m = text2.size();
+
+        vector<int> prev(m + 1, 0);
+        vector<int> curr(m + 1, 0);
+
+        for (int i = 1; i <= n; i++) {
+            for (int j = 1; j <= m; j++) {
+
+                if (text1[i - 1] == text2[j - 1])
+                    curr[j] = 1 + prev[j - 1];
+                else
+                    curr[j] = max(prev[j], curr[j - 1]);
+            }
+
+            prev = curr;
+        }
+
+        return prev[m];
+    }
+};
