730a575774d48f04631e61035dddd7693611b9d5
Gagandeep Y <gagan080305@gmail.com>
2026-08-10 09:05:35 +0530

    Add: target-sum - submission-1

---
+++ Data Structures & Algorithms/target-sum/submission-1.cpp
@@ -0,0 +1,22 @@
+class Solution {
+public://brute force - recurssion
+//T.C. - O(n*offset)
+// S.C. - O(n * totalSum) + O(n) recursion stack
+    int helper(int i,vector<int>& nums, int target,int sum,vector<vector<int>>&dp,int offset){
+        
+        if(i==nums.size())return (sum==target);
+        if(dp[i][sum+offset]!=-1)return dp[i][sum+offset];
+        int putPos = helper(i+1,nums,target,sum+nums[i],dp,offset);
+        int putNeg = helper(i+1,nums,target,sum-nums[i],dp,offset);
+        return dp[i][sum+offset] = putPos+putNeg;
+    }
+
+    int findTargetSumWays(vector<int>& nums, int target) {
+        int n = nums.size();
+        int totSum = 0;
+        for(int num : nums)totSum+=num;
+        vector<vector<int>>dp(n+1,vector<int>(2*totSum+1,-1));
+        //dp[i][sum]-number of different ways to reach target for curr sum starting from index i
+        return helper(0,nums,target,0,dp,totSum);
+    }
+};
